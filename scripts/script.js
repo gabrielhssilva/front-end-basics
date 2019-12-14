4c22b4801321fc07a346714089a69893f44aaa48
Gabriel Sousa <gabrielsousa@wizsolucoes.com.br>
2019-12-13 22:06:56 -0300

    Gerar números

--- scripts/script.js
+++ scripts/script.js
@@ -5,47 +5,47 @@ function validate(number) {
     } else if (number < 10 || number > 100) {
         alert('Por favor, insira um valor de 10 a 100!');
     } else {
-        drawIn('random-numbers', number);
-        // alert('Números Gerados');
+        generateNumbers();
     }
 }
 
-// let values = document.getElementById('generator').addEventListener('submit', drawIn);
-// let values = document.getElementById('generator');
-
-// function drawIn(divName, numbers) {
-//     let numero = document.getElementById('number').value;
-//     let div = document.getElementById('random-numbers');
-
-//     for (i = 1; i <= numero; i++) {
-//         let valor = Math.round(Math.random() * 100);
-
-//         if (i == (numero)) {
-//             div.innerHTML += valor;
-//         } else {
-//             div.innerHTML += valor + ' - ';
-//         }
-//     }
-
-//     console.log(div);
-// }
-
-function drawIn(divName, numbers) {
+// Desenhar números nas Div's
+function _drawIn(divName, numbers) {
     let div = document.getElementById(`${divName}`);
-
-    if (i == (qNumbers)) {
-        div.innerHTML += value;
-    } else {
-        div.innerHTML += value + ' - ';
-    }
-
-    console.log(div);
+    div.innerHTML = numbers;
+    console.log('Div do Draw:' + numbers);
 }
 
-function generateNumbers() {
-    qNumbers = document.getElementById('qNumbers').value;
 
-    for (i = 1; i <= qNumbers; i++) {
-        let numbers = Math.round(Math.random() * 100);
+// Gerar números aleatórios
+function generateNumbers(number) {
+
+    if (isNaN(number)) {
+        alert('Por favor, insira um valor numérico!');
+    } else if (number < 10 || number > 100) {
+        alert('Por favor, insira um valor de 10 a 100!');
+    } else {
+
+        let numbers = [];
+        qNumbers = document.getElementById('qNumbers').value;
+
+        for (i = 0; i < qNumbers; i++) {
+            numbers[i] = Math.round(Math.random() * 100);
+        }
+
+        // Números aleatórios
+        let randomNumbers = numbers;
+        _drawIn('random-numbers', randomNumbers);
+
+        // Números Crescentes
+        let increasingNumbers = numbers.sort((a, b) => { return a - b });
+        _drawIn('increasing-numbers', increasingNumbers);
+
+        // Números Decrescentes
+        let decreasingNumbers = numbers.sort((a, b) => { return b - a });
+        _drawIn('decreasing-numbers', decreasingNumbers);
+
     }
+
+
 }
