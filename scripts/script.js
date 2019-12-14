fbcbcc335d2a37c3337ffbb7ca1dcb759d72af03
Gabriel Sousa <gabrielsousa@wizsolucoes.com.br>
2019-12-13 23:57:53 -0300

    Desenhar números nas Divs

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,13 @@
-function validate(number) {
-
-    if (isNaN(number)) {
-        alert('Por favor, insira um valor numérico!');
-    } else if (number < 10 || number > 100) {
-        alert('Por favor, insira um valor de 10 a 100!');
-    } else {
-        generateNumbers();
-    }
-}
-
-// Desenhar números nas Div's
-function _drawIn(divName, numbers) {
-    let div = document.getElementById(`${divName}`);
-    div.innerHTML = numbers;
-    console.log('Div do Draw:' + numbers);
-}
-
-
 // Gerar números aleatórios
-function generateNumbers(number) {
+function generateNumbers() {
+    let numbers = [];
+    qNumbers = document.getElementById('qNumbers').value;
 
-    if (isNaN(number)) {
+    if (isNaN(qNumbers)) {
         alert('Por favor, insira um valor numérico!');
-    } else if (number < 10 || number > 100) {
+    } else if (qNumbers < 10 || qNumbers > 100) {
         alert('Por favor, insira um valor de 10 a 100!');
     } else {
-
-        let numbers = [];
-        qNumbers = document.getElementById('qNumbers').value;
 
         for (i = 0; i < qNumbers; i++) {
             numbers[i] = Math.round(Math.random() * 100);
@@ -45,7 +25,29 @@ function generateNumbers(number) {
         let decreasingNumbers = numbers.sort((a, b) => { return b - a });
         _drawIn('decreasing-numbers', decreasingNumbers);
 
+        // Numéros Ímpares Crescentes
+        _drawIn('creasing-odd',getCreasingOdd(numbers));
+
+        // Numéros Ímpares Crescentes
+        _drawIn('increasing-pairs',getIncreasingPairs(numbers));
     }
+}
 
+// Filtrar números Ímpares Crescentes
+function getCreasingOdd(numbers){
+    return numbers.filter(number => number % 2 !== 0 && number !== 0)
+        .sort((a, b) => a-b);
+}
 
+// Filtrar números Pares Decrescentes
+function getIncreasingPairs(numbers){
+    return numbers.filter(number => number % 2 == 0 && number !== 0)
+        .sort((a, b) => b-a )
+}
+
+// Desenhar números nas Div's
+function _drawIn(divName, numbers) {
+    let div = document.getElementById(`${divName}`);
+    div.innerHTML = (numbers !== null ? numbers : null);
+    console.log('Nome da Div -> ' + divName);
 }
